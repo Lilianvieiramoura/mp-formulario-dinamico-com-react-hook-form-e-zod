7108bbb2f09b6669ad51360c4e5a84cdc01023e0
Lílian Vieira <lilianv528@gmail.com>
2025-04-01 13:59:20 -0300

    react hook form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,44 @@
-import { EyeIcon } from 'lucide-react';
-// import { EyeOffIcon } from 'lucide-react';
+import { EyeIcon, Loader } from 'lucide-react';
+import { useState } from 'react';
+import { EyeOffIcon } from 'lucide-react';
+import { withMask } from 'use-mask-input';
+import { FieldValues, useForm} from 'react-hook-form';
 
 export default function Form() {
+
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [address, setAddress] = useState({city: '', street: ''});
+  const {handleSubmit, register, formState: {isSubmitting}} = useForm();
+
+  async function handleZipcodeBlur(e: React.FocusEvent<HTMLInputElement>) {
+
+  const zipcode = e.target.value;
+  const res = await fetch(`https://brasilapi.com.br/api/cep/v2/${zipcode}`)
+
+  if (res.ok) {
+    const data = await res.json();
+    setAddress({
+      city: data.city,
+      street: data.street,
+    });
+  }
+}
+
+  async function onSubmit(data: FieldValues) {
+    console.log('form submitted');
+    console.log(data);
+    
+    const res = await fetch('https://apis.codante.io/api/register-user/register', {method: 'POST', body: JSON.stringify(data)})
+    const datas = await res.json();
+    console.log(datas);
+    
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-4">
         <label htmlFor="name">Nome Completo</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" {...register('name')} />
         {/* Sugestão de exibição de erro de validação */}
         <div className="min-h-4">
           <p className="text-xs text-red-400 mt-1">O nome é obrigatório.</p>
@@ -14,45 +46,57 @@ export default function Form() {
       </div>
       <div className="mb-4">
         <label htmlFor="email">E-mail</label>
-        <input className="" type="email" id="email" />
+        <input className="" type="email" id="email" {...register('email')}/>
       </div>
       <div className="mb-4">
         <label htmlFor="password">Senha</label>
         <div className="relative">
-          <input type="password" id="password" />
+          <input type={isPasswordVisible ? 'text' : 'password'} id="password" {...register('password')}/>
           <span className="absolute right-3 top-3">
-            <EyeIcon size={20} className="text-slate-600 cursor-pointer" />
-            {/* <EyeOffIcon
+            <button type='button' onClick={() => setIsPasswordVisible(!isPasswordVisible)}>
+
+              {isPasswordVisible ? (
+                <EyeIcon size={20} className="text-slate-600 cursor-pointer" />
+              ) : (
+                <EyeOffIcon
                       className="text-slate-600 cursor-pointer"
                       size={20}
-                    /> */}
+                    />
+              )}
+            </button>
           </span>
         </div>
       </div>
       <div className="mb-4">
         <label htmlFor="confirm-password">Confirmar Senha</label>
         <div className="relative">
-          <input type="password" id="confirm-password" />
+          <input type={isPasswordVisible ? 'text' : 'password'} id="confirm-password" />
           <span className="absolute right-3 top-3">
-            <EyeIcon size={20} className="text-slate-600 cursor-pointer" />
-            {/* <EyeOffIcon
-                  className="text-slate-600 cursor-pointer"
-                  size={20}
-                /> */}
+          <button type='button' onClick={() => setIsPasswordVisible(!isPasswordVisible)}>
+
+            {isPasswordVisible ? (
+              <EyeIcon size={20} className="text-slate-600 cursor-pointer" />
+            ) : (
+              <EyeOffIcon
+                    className="text-slate-600 cursor-pointer"
+                    size={20}
+                  />
+            )}
+            </button>
           </span>
         </div>
       </div>
       <div className="mb-4">
         <label htmlFor="phone">Telefone Celular</label>
-        <input type="text" id="phone" />
+        <input type="text" id="phone" ref={withMask('(99) 99999-9999')}/>
       </div>
       <div className="mb-4">
         <label htmlFor="cpf">CPF</label>
-        <input type="text" id="cpf" />
+        <input type="text" id="cpf" ref={withMask('cpf')}/>
       </div>
       <div className="mb-4">
         <label htmlFor="cep">CEP</label>
-        <input type="text" id="cep" />
+        <input type="text" id="cep" ref={withMask('99999-999')} onBlur={handleZipcodeBlur}/>
       </div>
       <div className="mb-4">
         <label htmlFor="address">Endereço</label>
@@ -60,7 +104,8 @@ export default function Form() {
           className="disabled:bg-slate-200"
           type="text"
           id="address"
-          disabled
+          // disabled
+          value={address.street}
         />
       </div>
 
@@ -71,6 +116,7 @@ export default function Form() {
           type="text"
           id="city"
           disabled
+          value={address.city}
         />
       </div>
       {/* terms and conditions input */}
@@ -89,9 +135,11 @@ export default function Form() {
 
       <button
         type="submit"
-        className="bg-slate-500 font-semibold text-white w-full rounded-xl p-4 mt-10 hover:bg-slate-600 transition-colors"
+        disabled={isSubmitting}
+        className="bg-slate-500 font-semibold text-white w-full rounded-xl p-4 mt-10 hover:bg-slate-600 transition-colors disabled:bg-slate-300 flex items-center justify-center"
       >
-        Cadastrar
+        {isSubmitting ? <Loader className='animate-spin' /> : 'Cadastrar' }
+        
       </button>
     </form>
   );
